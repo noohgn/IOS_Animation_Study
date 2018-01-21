278615868e937bbc596c1eebd7a5d4cf68aa5ceb
LeeJeonghun <spits1968@gmail.com>
2018-01-21 16:45:59 +0900

    Animating user interactions

--- section2/BahamaAirLoginScreen/ViewController.swift
+++ section2/BahamaAirLoginScreen/ViewController.swift
@@ -124,12 +124,23 @@ class ViewController: UIViewController {
         self.loginButton.center.y -= 30
         self.loginButton.alpha = 1.0
     }, completion: nil)
+    
   }
   
   // MARK: further methods
-  
   @IBAction func login() {
     view.endEditing(true)
+    UIView.animate(withDuration: 1.5, delay: 0.0, usingSpringWithDamping: 0.2, initialSpringVelocity: 0.0, options: [], animations: {
+        self.loginButton.bounds.size.width += 80.0
+        
+    }, completion: nil)
+    UIView.animate(withDuration: 0.33, delay: 0.0, usingSpringWithDamping: 0.7, initialSpringVelocity: 0.0, options: [], animations: {
+        self.loginButton.center.y += 60.0
+        self.loginButton.backgroundColor = UIColor(red: 0.85, green: 0.83, blue: 0.45, alpha: 1.0)
+        self.spinner.center = CGPoint(x: 40.0, y:self.loginButton.frame.size.height/2)
+        self.spinner.alpha = 1.0
+    }, completion: nil)
+    
   }
   
   // MARK: UITextFieldDelegate
