def2da1aded1b6e281390a18fee058401861ed55
LeeJeonghun <spits1968@gmail.com>
2018-01-22 00:32:32 +0900

    section4 Cube transitions

--- section4/Flight Info/ViewController.swift
+++ section4/Flight Info/ViewController.swift
@@ -28,8 +28,13 @@ func delay(seconds: Double, completion: @escaping ()-> Void) {
   DispatchQueue.main.asyncAfter(deadline: .now() + seconds, execute: completion)
 }
 
+
 class ViewController: UIViewController {
-  
+    enum AnimationDirection: Int{
+        case positive = 1
+        case negative = -1
+    }
+
   @IBOutlet var bgImageView: UIImageView!
   
   @IBOutlet var summaryIcon: UIImageView!
@@ -68,7 +73,7 @@ class ViewController: UIViewController {
   
   //MARK: custom methods
   
-  func changeFlight(to data: FlightData) {
+    func changeFlight(to data: FlightData ,animated: Bool = false) {
     
     // populate the UI with the next flight's data
     summary.text = data.summary
@@ -77,13 +82,54 @@ class ViewController: UIViewController {
     departingFrom.text = data.departingFrom
     arrivingTo.text = data.arrivingTo
     flightStatus.text = data.flightStatus
-    bgImageView.image = UIImage(named: data.weatherImageName)
-    snowView.isHidden = !data.showWeatherEffects
+        if animated {
+            fade(imageView: bgImageView, toImage: UIImage(named: data.weatherImageName)!, showEffect: data.showWeatherEffects)
+            let direction: AnimationDirection = data.isTakingOff ? .positive : .negative
+            cubeTrasition(label: flightNr, text: data.flightNr, direction: direction)
+            cubeTrasition(label: gateNr, text: data.gateNr, direction: direction)
+        } else {
+            bgImageView.image = UIImage(named: data.weatherImageName)
+            snowView.isHidden = !data.showWeatherEffects
+            
+            flightNr.text = data.flightNr
+            gateNr.text = data.gateNr
+            
+            departingFrom.text = data.departingFrom
+            arrivingTo.text = data.arrivingTo
+            
+            flightStatus.text = data.flightStatus
+        }
     
     // schedule next flight
     delay(seconds: 3.0) {
-      self.changeFlight(to: data.isTakingOff ? parisToRome : londonToParis)
+      self.changeFlight(to: data.isTakingOff ? parisToRome : londonToParis, animated: true)
     }
   }
-
+  
+    func fade(imageView: UIImageView, toImage: UIImage, showEffect: Bool) {
+        UIView.transition(with: imageView, duration: 1.0, options: .transitionCrossDissolve, animations: {
+            imageView.image = toImage
+        }, completion: nil)
+    }
+    
+    func cubeTrasition(label: UILabel, text: String, direction: AnimationDirection ) {
+        let auxLabel = UILabel(frame: label.frame)
+        auxLabel.text = text
+        auxLabel.font = label.font
+        auxLabel.textAlignment = label.textAlignment
+        auxLabel.textColor = label.textColor
+        auxLabel.backgroundColor = label.backgroundColor
+        
+        let auxLabelOffset = CGFloat(direction.rawValue) * label.frame.size.height/2.0
+        auxLabel.transform = CGAffineTransform(translationX: 0.0, y: auxLabelOffset).scaledBy(x: 1.0, y: 0.1)
+        label.superview?.addSubview(auxLabel)
+        UIView.animate(withDuration: 0.5, delay: 0.0, options: .curveEaseOut, animations: {
+            auxLabel.transform = .identity
+            label.transform = CGAffineTransform(translationX: 0.0, y: -auxLabelOffset).scaledBy(x: 1.0, y: 0.1)
+        }, completion: { _ in
+            label.text = auxLabel.text
+            label.transform = .identity
+            auxLabel.removeFromSuperview()
+        })
+    }
 }
